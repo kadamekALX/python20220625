5f6eef789c7c30538a97b913c5edc49e8380fce8
Krzysztof Adamek <kadamek@alx.pl>
2022-06-25 15:08:56 +0200

    if

---
+++ basics/instrukcja_warunkowa.py
@@ -0,0 +1,17 @@
+liczba = 5
+
+if liczba % 2 == 0:
+    print("To się wypisze jeśli warunek będzie == True")
+    print("liczba jest parzysta")
+else:
+    print("To się wypisze jeśli warunek będzie == False")
+    print("liczba jest nieparzysta")
+
+print("To jest poza ifem")
+
+# if war1:
+#     pass
+# elif war2:
+#     pass
+# else:
+#     pass
---
+++ basics/zadanie_if.py
@@ -0,0 +1,15 @@
+liczba = int(input("Podaj liczbę:"))
+
+# if liczba < 0:
+#     print("Liczba jest < 0")
+# else:
+#     if liczba > 0:
+#         print("Liczba jest > 0")
+#     else:
+#         print("Liczba jest == 0")
+if liczba < 0:
+    print("Liczba jest < 0")
+elif liczba > 0:
+    print("Liczba jest > 0")
+else:
+    print("Liczba jest == 0")
