d8dbb9677532f892778dc608b0c0030d2a6053e2
Krzysztof Adamek <kadamek@alx.pl>
2022-06-26 16:48:46 +0200

    hopsasa v2

--- collections/hopsasa.py
+++ collections/hopsasa.py
@@ -1,9 +1,16 @@
+# zamiast podzielnych przez 7 wypisz "Bum" (i wszystkie zasady wynikające z tego)
+
+# print("asdf", end="")
+# print("qwer")
+
 for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("HopsasaTralala")
-    elif i % 3 == 0:
-        print("Hopsasa")
-    elif i % 5 == 0:
-        print("Tralala")
+    napis = ""
+    if i % 3 == 0:
+        napis += "Hopsasa"
+    if i % 5 == 0:
+        napis += "Tralala"
+
+    if napis:  # czy napis jest niepusty
+        print(napis)
     else:
         print(i)
