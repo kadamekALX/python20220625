866cf2a685e06acc5a3fd2c1ac857ebcf1e99948
Krzysztof Adamek <kadamek@alx.pl>
2022-06-26 10:23:23 +0200

    pętla while

---
+++ basics/petla_while.py
@@ -0,0 +1,8 @@
+# petla nieskończona
+# while True:
+#     print("OK")
+
+licznik = 0  # liczy ile razy została wykonana zawartość pętli
+while licznik < 5:
+    print("OK", licznik)
+    licznik += 1  # to samo co: licznik = licznik + 1
