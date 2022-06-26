8b8b9302b5265b47d8eee3eb6591d0d9d8ab7224
Krzysztof Adamek <kadamek@alx.pl>
2022-06-26 14:51:02 +0200

    kolekcje - zad 1

---
+++ collections/zadanie_1.py
@@ -0,0 +1,12 @@
+x = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+
+# - drugi element
+print(f"{x[1] = }")
+# - przedostatni element
+print(f"{x[-2] = }")
+# - elementy od trzeciego do siódmego (włącznie)
+print(f"{x[2:7] = }")
+# - co trzeci element
+print(f"{x[::3] = }")
+# - co drugi element idąc od końca
+print(f"{x[::-2] = }")
