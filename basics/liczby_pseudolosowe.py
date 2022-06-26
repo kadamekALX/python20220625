9896f9505dfc75bc75fbda8bf41132f8fc19a0de
Krzysztof Adamek <kadamek@alx.pl>
2022-06-26 12:14:27 +0200

    random

---
+++ basics/liczby_pseudolosowe.py
@@ -0,0 +1,11 @@
+# importujemy funkcję `randint` z modułu `random`
+# from random import randint
+# x = randint(1, 5)
+# print(x)
+
+import random
+
+x = random.randint(1, 5)
+print(x)
+
+print(random.random())
