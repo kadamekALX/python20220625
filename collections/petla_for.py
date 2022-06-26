3378a3a0c00b26470e8f56709d4b6b59bd947250
Krzysztof Adamek <kadamek@alx.pl>
2022-06-26 15:43:26 +0200

    pętla for

---
+++ collections/petla_for.py
@@ -0,0 +1,18 @@
+lista = [10, 20, 30]
+
+for x in lista:
+    print(f"{x = }")
+
+for i in range(5):
+    print(f"{i = }")
+
+print()
+
+for i in range(6, 10):
+    print(f"{i = }")
+
+print()
+
+for i in range(6, 15, 3):
+    print(f"{i = }")
+
