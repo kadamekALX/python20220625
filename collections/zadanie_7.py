ee056b5ff53fe3956d47c305b0ad02054a9ae643
Krzysztof Adamek <kadamek@alx.pl>
2022-07-16 12:13:23 +0200

    zad 7.

--- collections/zadanie_7.py
+++ collections/zadanie_7.py
@@ -3,7 +3,8 @@ Napisz program zliczający liczbę znaków w podanym przez użytkownika napisie
 pomiędzy nawiasami <>. Nawiasy mogą wystąpić tylko raz.
 
 Przykład:
-Ala ma <kota>, a kot ma Alę
+0123456789
+Ala <kota>, a kot ma Alę
 4
 1. Sprawdzam liczbę < i > - powinno ich być po jednym,
       jeżeli liczba tych nawiasów jest inna, to kończę program
@@ -13,3 +14,21 @@ Ala ma <kota>, a kot ma Alę
       - czy zliczanie jest włączone i wtedy zliczam
 """
 
+napis = input('Podaj napis: ')
+
+if napis.count('<') != 1 or napis.count('>') != 1:
+    print('Niepoprawna liczba nawiasow!')
+    exit()
+
+liczba_znakow = 0
+czy_zliczac = False
+
+for znak in napis:
+    if znak == '<':
+        czy_zliczac = True
+    elif znak == '>':
+        czy_zliczac = False
+    elif czy_zliczac == True:
+        liczba_znakow += 1
+
+print(liczba_znakow)
