31afadab23b71baf5da7b41a478e683ba7e0996d
Krzysztof Adamek <kadamek@alx.pl>
2022-07-16 14:29:59 +0200

    zad 8.

--- collections/zadanie_8.py
+++ collections/zadanie_8.py
@@ -16,3 +16,23 @@ Wypisz wszystkie dostępne produkty w sklepie.
 5. Liczymy należność
 """
 
+produkty = {
+    'ziemniaki': 1.2,
+    'pomidory': 4.5,
+    'marchew': 0.5,
+}
+
+print('Lista produktow')
+for produkt, cena in produkty.items():
+    print(f'{produkt} - {cena:.2f} zł / kg')
+
+co = input('Co chcesz kupić? ')
+
+if co not in produkty:
+    print('Nie mamy takiego produktu w sklepie!')
+    exit()  # powoduje zamkniecie programu
+
+liczba_kg = float(input(f'Ile kg produktu {co} chcesz kupic? '))
+naleznosc = produkty[co] * liczba_kg
+
+print(f'Za {liczba_kg} kg produktu {co} zapłacisz {naleznosc:.2f} zł.')
