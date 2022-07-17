4a6119ebb1ca6f21f48b7fcfeae946a758649018
Krzysztof Adamek <kadamek@alx.pl>
2022-07-17 11:59:54 +0200

    zadanie 2.

---
+++ functions/zadanie_2.py
@@ -0,0 +1,58 @@
+"""
+Napisz funkcję sprawdzającą, czy dana liczba jest liczbą pierwszą.
+Przykład użycia:
+> czy_jest_pierwsza(10)
+False
+> czy_jest_pierwsza(17)
+True
+
+Liczba pierwsza
+- bedzie intem
+- wieksza od 1
+- dzieli sie tylko przez 1 i przez sama siebie
+"""
+
+
+def czy_jest_pierwsza(liczba: int) -> bool:
+    """Funkcja sprawdza czy liczba jest pierwsza.
+
+    https://pl.wikipedia.org/wiki/Liczba_pierwsza
+
+    :param liczba:
+    :return:
+    """
+    if liczba <= 1:
+        return False
+
+    for i in range(2, liczba):
+        if liczba % i == 0:
+            return False
+
+    return True
+
+
+def test_czy_liczba_jest_pierwsza():
+    # GWT - https://martinfowler.com/bliki/GivenWhenThen.html
+    # Given
+    liczba = 5
+
+    # When
+    wynik = czy_jest_pierwsza(liczba)
+
+    # Then
+    # asercja (ang. assert) - uznanie jakiegos zdania/wyrazenia za prawdziwe
+    # https://pl.wikipedia.org/wiki/Asercja_(informatyka)
+    # https://sjp.pl/asercja
+    assert wynik == True
+
+
+def test_wielu_liczb_pierwszych():
+    liczby = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    for liczba in liczby:
+        assert czy_jest_pierwsza(liczba) == True
+
+
+def test_wielu_liczb_nie_pierwszych():
+    liczby = [-100, -10, 0, 1, 4, 8, 9, 30, 50, 100]
+    for liczba in liczby:
+        assert czy_jest_pierwsza(liczba) == False
