1a141c69e93a51a946bb8e51e3ccf08c2d125d07
Krzysztof Adamek <kadamek@alx.pl>
2022-07-17 10:14:58 +0200

    funckje, zadanie 1.

---
+++ functions/intro.py
@@ -0,0 +1,68 @@
+# Funkcje
+
+# definicja funkcji
+def powiedz_czesc():
+    print('Cześć!')
+
+# uruchomienie funkcji
+powiedz_czesc()
+
+# jezeli funkcja potrzebuje danych do pracy
+# powinnismy je przekazac jako argumenty
+def powiedz_czesc_komus(imie, nazwisko):
+    print(f'Cześć {imie} {nazwisko}!')
+
+
+powiedz_czesc_komus('Piotr', 'GG')
+
+moje_imie = 'Jan'
+moje_nazwisko = 'Kowalski'
+powiedz_czesc_komus(moje_imie, moje_nazwisko)
+
+# moje_imie = input('Podaj imie: ')
+# moje_nazwisko = input('Podaj nazwisko: ')
+# powiedz_czesc_komus(moje_imie, moje_nazwisko)
+
+
+def suma(a, b):
+    return a + b
+    # print('Ala ma kota')  # nic po returnie sie nie wykona
+
+
+wynik = suma(2, 3)
+print(wynik)
+
+
+def czy_duza_liczba(liczba):
+    if liczba > 1000:
+        return 'duza'
+    else:
+        return 'mala'
+
+wynik = czy_duza_liczba(1)
+print(wynik)
+
+wynik = czy_duza_liczba(2000)
+print(wynik)
+
+
+# Dokumentowanie funkcji
+# type hinting
+# opis dzialania funkcji
+# https://realpython.com/documenting-python-code/
+def suma(a: int, b: int) -> int:
+    """Dodaje dwie liczby i zwaraca ich sumę.
+
+    Tutaj mozemy opisać w szczegółach jak nasza funkcja działa.
+    Co powinniśmy przekazywać, jak, czego się spodziewać.
+
+    Ala ma kota.
+
+    :param a: pierwsza liczba
+    :param b: druga liczba
+    :return: wynik dodawania a i b
+    """
+    return a + b
+
+
+print(suma(3, 4))
