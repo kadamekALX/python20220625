e63c165ed0b18e350640cd9acd9da7a499d1f8cc
Krzysztof Adamek <kadamek@alx.pl>
2022-07-31 10:12:13 +0200

    klasy - Postac

--- oop/postac.py
+++ oop/postac.py
@@ -1,21 +1,28 @@
 class Postac:
     def __init__(self, imie, zdrowie):
-        pass
+        self.imie = imie
+        self.zdrowie = zdrowie
+        self.max_zdrowie = zdrowie
 
     # Postac traci `ile` punktow zdrowia. Zdrowie nie powinno spaść poniżej 0.
     def otrzymaj_obrazenia(self, ile):
-        pass
+        self.zdrowie -= ile
+        if self.zdrowie < 0:
+            self.zdrowie = 0
 
     # np. "Rufus, 57/100 HP"
     def wypisz_info(self):
-        pass
+        print(f"{self.imie}, zdrowie: {self.zdrowie}/{self.max_zdrowie}")
 
     def czy_zyje(self):
-        pass
+        return self.zdrowie > 0
 
     # przywraca `ile` punktów zdrowia. Zdrowie nie powinno przekroczyć maksymalnej wartości.
     def wylecz(self, ile):
-        pass
+        if self.czy_zyje():
+            self.zdrowie += ile
+            if self.zdrowie > self.max_zdrowie:
+                self.zdrowie = self.max_zdrowie
 
 
 p = Postac("Rufus", 120)
