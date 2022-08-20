1d71f16191d240906544bf4217bb43418a454294
Krzysztof Adamek <kadamek@alx.pl>
2022-08-20 10:41:38 +0200

    klasy - Robot v3

--- oop/robot.py
+++ oop/robot.py
@@ -12,10 +12,12 @@
 # - ciąg instrukcji: NNPNLNPP,
 # - końcowe położenie robota: (1, 3), zwrot: S.
 
-kierunki = ("N", "E", "S", "W")
-ruchy = ((0, 1), (1, 0), (0, -1), (-1, 0))
 
 class Robot:
+    # pola klasowe
+    kierunki = ("N", "E", "S", "W")
+    ruchy = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
     def __init__(self):
         self.x = 0
         self.y = 0
@@ -28,11 +30,11 @@ class Robot:
         self.zwrot = (self.zwrot + 3) % 4
 
     def naprzod(self):
-        self.x += ruchy[self.zwrot][0]
-        self.y += ruchy[self.zwrot][1]
+        self.x += Robot.ruchy[self.zwrot][0]
+        self.y += Robot.ruchy[self.zwrot][1]
 
     def wypisz(self):
-        print(f"x={self.x}, y={self.y}, zwrot: {kierunki[self.zwrot]}")
+        print(f"x={self.x}, y={self.y}, zwrot: {Robot.kierunki[self.zwrot]}")
 
     def wykonaj(self, ciag_instrukcji):
         metody = {'N': self.naprzod, 'P': self.prawo, 'L': self.lewo}
