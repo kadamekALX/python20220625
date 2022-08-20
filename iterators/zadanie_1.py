d87f9f282db33c0e30497d2371e135cbc3daffb4
Krzysztof Adamek <kadamek@alx.pl>
2022-08-20 12:11:51 +0200

    iteratory - zadanie 1 - osobna klasa na iterator

--- iterators/zadanie_1.py
+++ iterators/zadanie_1.py
@@ -1,17 +1,21 @@
+class CounterIterator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __next__(self):
+        if self.a == self.b:
+            raise StopIteration
+        w = self.a
+        self.a += 1
+        return w
+
 class Counter:
     def __init__(self, limit):
         self.limit = limit
 
     def __iter__(self):
-        self.licznik = 0
-        return self
-
-    def __next__(self):
-        if self.licznik > self.limit:
-            raise StopIteration
-        wynik = self.licznik
-        self.licznik += 1
-        return wynik
+        return CounterIterator(0, self.limit)
 
 
 for x in Counter(5):
