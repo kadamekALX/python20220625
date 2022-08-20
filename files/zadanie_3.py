61b46de2649b762dbd9b26ca01e637257af36d69
Krzysztof Adamek <kadamek@alx.pl>
2022-08-20 16:16:02 +0200

    pliki - zadanie 3 v2

--- files/zadanie_3.py
+++ files/zadanie_3.py
@@ -6,16 +6,19 @@
 # Wynikowy plik powinien zawierać unikalne, posortowane, poprawne
 # adresy email.
 
-with open("emails.txt") as plik:
-    maile = set()
-    for wiersz in plik:
-        if wiersz.count("@") != 1:
-            continue
-        wiersz = wiersz.lower()
-        wiersz = wiersz.strip()  # usuwa białe znaki z początku i końca napisu
-        maile.add(wiersz)
+# with open("emails.txt") as plik:
+#     maile = set()
+#     for wiersz in plik:
+#         if wiersz.count("@") != 1:
+#             continue
+#         wiersz = wiersz.lower()
+#         wiersz = wiersz.strip()  # usuwa białe znaki z początku i końca napisu
+#         maile.add(wiersz)
+#
+# with open("clean_emails.txt", "w") as plik:
+#     for x in sorted(maile):
+#         plik.write(x)
+#         plik.write('\n')
 
-with open("clean_emails.txt", "w") as plik:
-    for x in sorted(maile):
-        plik.write(x)
-        plik.write('\n')
+with open("emails.txt") as plik_in, open("clean_emails.txt", "w") as plik_out:
+    plik_out.write("\n".join(sorted({w.lower().strip() for w in plik_in if w.count("@") == 1})))
